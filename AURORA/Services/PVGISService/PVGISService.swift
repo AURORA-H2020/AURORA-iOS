7c8a78a4e5696c996b990c32aa7b66ab89b6c3ea
Sven Tiigi <sven.tiigi@gmail.com>
2023-03-08 08:53:51 +0100

    Updated PVGISService

--- AURORA/Services/PVGISService/PVGISService.swift
+++ AURORA/Services/PVGISService/PVGISService.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 /// The PVGIS (Photovoltaic Geographical Information System) Service.
-struct PVGISService {
+struct PVGISService: Sendable {
     
     // MARK: Properties
     
@@ -36,26 +36,115 @@ extension PVGISService {
     
 }
 
+// MARK: - PhotovoltaicInvestmentResult
+
+extension PVGISService {
+    
+    /// A photovoltaic investment result
+    struct PhotovoltaicInvestmentResult: Codable, Hashable {
+        
+        /// The produced energy by the photovoltaic.
+        let producedEnergy: Measurement<UnitEnergy>
+    
+        /// The carbon emissions produced by the photovoltaic.
+        let carbonEmissions: Measurement<UnitMass>
+        
+        /// The carbon emissions without the photovoltaic.
+        let normalCarbonEmissions: Measurement<UnitMass>
+        
+        /// The carbon emissions reduction.
+        var carbonEmissionsReduction: Measurement<UnitMass> {
+            self.normalCarbonEmissions - self.carbonEmissions
+        }
+        
+    }
+    
+}
+
 // MARK: - Calculcate Photovoltaic Investment
 
 extension PVGISService {
     
-    struct PhotovoltaicInvestmentResult: Codable, Hashable {
-        
-        let producedEnergy: Double
-    
-        let carbonEmissionsSavings: Double
-        
-    }
-    
+    /// Calculate a photovoltaic investment.
+    /// - Parameters:
+    ///   - amount: The amount to invest.
+    ///   - pvgisParams: The PVGIS parameters of the city.
+    ///   - country: The country.
     func calculcatePhotovoltaicInvestment(
         amount: Double,
         using pvgisParams: City.PVGISParams,
         in country: Country
     ) async throws -> PhotovoltaicInvestmentResult {
-        guard var urlComponents = URLComponents(url: self.hostURL, resolvingAgainstBaseURL: false) else {
+        // Try to retrieve response
+        let (responseData, response) = try await self.urlSession
+            .data(
+                from: .init(
+                    url: self.hostURL,
+                    amount: amount,
+                    pvgisParams: pvgisParams
+                )
+            )
+        // Verify response data is not empty
+        // and the response is a success
+        guard !responseData.isEmpty,
+              (response as? HTTPURLResponse)?.statusCode == 200 else {
+            // Otherwise throw an error
+            throw URLError(.badServerResponse)
+        }
+        // Try to decode response as calculation response
+        let calculcationResponse = try JSONDecoder()
+            .decode(
+                CalculcationResponse.self,
+                from: responseData
+            )
+        // Initialize the produced energy
+        let producedEnergy = Measurement<UnitEnergy>(
+            value: calculcationResponse
+                .outputs
+                .totals
+                .fixed
+                .energyOutputPerYear,
+            unit: .kilowattHours
+        )
+        // Return photovoltaic investment result
+        return .init(
+            producedEnergy: producedEnergy,
+            carbonEmissions: .init(
+                value: producedEnergy.value * 0.0305,
+                unit: .kilograms
+            ),
+            normalCarbonEmissions: .init(
+                value: producedEnergy.value * 0.116,
+                unit: .kilograms
+            )
+        )
+    }
+    
+}
+
+// MARK: - URL+init
+
+private extension URL {
+    
+    /// Creates a new instance of `URL`.
+    /// - Parameters:
+    ///   - url: The host URL.
+    ///   - amount: The amount.
+    ///   - pvgisParams: The PVGIS parameters.
+    init(
+        url: URL,
+        amount: Double,
+        pvgisParams: City.PVGISParams
+    ) throws {
+        // Verify URLComponents can be initialized from URL.
+        guard var urlComponents = URLComponents(
+            url: url,
+            resolvingAgainstBaseURL: false
+        ) else {
+            // Otherwise throw an error
             throw URLError(.badURL)
         }
+        // Set query items
         urlComponents.queryItems = [
             .init(
                 name: "lat",
@@ -98,67 +187,77 @@ extension PVGISService {
                 value: "json"
             )
         ]
+        // Verify a URL is available
         guard let url = urlComponents.url else {
+            // Otherwise throw an error
             throw URLError(.badURL)
         }
-        let (responseData, response) = try await self.urlSession.data(from: url)
-        print(String(decoding: responseData, as: UTF8.self))
-        guard !responseData.isEmpty, (response as? HTTPURLResponse)?.statusCode == 200 else {
-            throw URLError(.badServerResponse)
-        }
-        let calculcationResponse = try JSONDecoder()
-            .decode(
-                CalculcationResponse.self,
-                from: responseData
-            )
-        
-        throw URLError(.badURL)
+        // Initialize
+        self = url
     }
     
 }
 
+// MARK: - CalculcationResponse
+
 private extension PVGISService {
     
+    /// A PVGIS calculcation response
     struct CalculcationResponse: Codable {
         
+        /// The Outputs.
         let outputs: Outputs
         
     }
     
 }
 
+// MARK: - CalculcationResponse+Outputs
+
 private extension PVGISService.CalculcationResponse {
     
+    /// The PVGIS calculcation response outputs.
     struct Outputs: Codable {
         
+        /// The Totals.
         let totals: Totals
         
     }
     
 }
 
+// MARK: - CalculcationResponse+Outputs+Totals
+
 private extension PVGISService.CalculcationResponse.Outputs {
     
+    /// The PVGIS calculcation response outputs totals.
     struct Totals: Codable {
         
+        /// The Fixed.
         let fixed: Fixed
         
     }
     
 }
 
+// MARK: - CalculcationResponse+Outputs+Totals+Fixed
+
 private extension PVGISService.CalculcationResponse.Outputs.Totals {
     
+    /// The PVGIS calculcation response outputs totals fixed.
     struct Fixed: Codable {
         
+        /// The CodingKeys
         // swiftlint:disable:next nesting
         private enum CodingKeys: String, CodingKey {
             case energyOutputPerMonth = "E_m"
             case energyOutputPerYear = "E_y"
         }
         
+        /// The energy output per month.
         let energyOutputPerMonth: Double
         
+        /// The energy output per year.
         let energyOutputPerYear: Double
         
     }
