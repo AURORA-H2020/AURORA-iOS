65ed5e88ca251be6315b33b3c5cbbfb3e8ff6536
Sven Tiigi <sven.tiigi@gmail.com>
2024-12-19 10:40:45 +0100

    Add documentation

--- AURORA/Models/City/City+PVGISParams.swift
+++ AURORA/Models/City/City+PVGISParams.swift
@@ -7,14 +7,19 @@ extension City {
     /// The PVGIS (Photovoltaic Geographical Information System) parameters of a city.
     struct PVGISParams: Codable, Hashable {
         
+        /// The angle.
         let angle: Double
         
+        /// The aspect.
         let aspect: Double
         
+        /// The investment factor.
         let investmentFactor: Double
         
+        /// The latitude.
         let lat: Double
         
+        /// The longitude.
         let long: Double
         
     }
